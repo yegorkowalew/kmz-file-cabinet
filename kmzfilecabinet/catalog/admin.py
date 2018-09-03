d6fada1f114965854de6a13beebd60aa3d77d3fb
Коваленко Ігор Сергійович <i.kovalenko@kmzindustries.ua>
2018-09-03 16:57:11 +0300

    переработка моделей

--- kmzfilecabinet/catalog/admin.py
+++ kmzfilecabinet/catalog/admin.py
@@ -2,14 +2,33 @@ from django.contrib import admin
 from django.db import models
 # Register your models here.
 
-from catalog.models import Unit, PreName
-# admin.site.register()
+from .models import Unit, PreName, Group, Membership
+
+class TermInlineAdmin(admin.TabularInline):
+    model = Group.members.through
+
 class UnitAdmin(admin.ModelAdmin):
-    # pass
-    fields = (('prename', 'name'), ('stuff', 'titul_file'), 'comment')
+    fields = (('prename', 'name'), ('titul_file'), 'comment')
     list_display = ('name', 'prename', 'edit_date')
     search_fields = ['name']
+    # inlines = (TermInlineAdmin,)
+
+
+class GroupAdmin(admin.ModelAdmin):
+    fields = ('name',)
+    inlines = (TermInlineAdmin,)
+    # list_display = ('name', 'prename', 'edit_date')
+    # search_fields = ['name']
+
+class MembershipAdmin(admin.ModelAdmin):
+    fields = ('group', 'unit', 'inviter')
+    
+    # list_display = ('name', 'prename', 'edit_date')
+    # search_fields = ['name']
+
 
 admin.site.register(Unit, UnitAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Membership, MembershipAdmin)
 
 admin.site.register(PreName)
--- kmzfilecabinet/catalog/models.py
+++ kmzfilecabinet/catalog/models.py
@@ -8,12 +8,13 @@ class PreName(models.Model):
                             )
     def __str__(self):
             return self.name
-
     class Meta:
         ordering = ["name"]
         verbose_name_plural = "Тип узла"
 
+
 class Unit(models.Model):
+    name = models.CharField(max_length=50)
     prename = models.ForeignKey(
                             'PreName',
                             verbose_name="Тип узла", 
@@ -26,33 +27,41 @@ class Unit(models.Model):
                             max_length=30,
                             help_text="Используйте формат: <em>XXX.XXX.XXX.XXX</em>."
                             )
-    stuff = models.ManyToManyField(
-                            'self', 
-                            verbose_name="Вложенные узлы", 
-                            blank = True, 
-                            symmetrical=False
-                            )
     edit_date = models.DateField(
                             verbose_name="Последнее изменение", 
                             auto_now=True,
                             )
-    
     titul_file = models.FileField(
                             verbose_name="Фото титульной страницы", 
                             blank = True, 
     )
-
     comment = models.TextField(
                             verbose_name="Комментарий", 
                             blank = True, 
     )
-
     def __unicode__(self):
         return self.name % self.id
-
     def __str__(self):
             return self.name+' ('+self.prename.name+')'
-
     class Meta:
         ordering = ["name"]
         verbose_name_plural = "Узлы"
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(
+        'Group',
+        through='Membership',
+        # through_fields=('group',),
+    )
+
+class Membership(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    # group2 = models.ForeignKey(Group, on_delete=models.CASCADE)
+    # unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    inviter = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        related_name="membership_group",
+    )
+    invite_reason = models.CharField(max_length=64)
