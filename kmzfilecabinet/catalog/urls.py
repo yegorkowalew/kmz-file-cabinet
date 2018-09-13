94a1978cd8cc27db2a4c297fe212ce74b9f51e31
Коваленко Ігор Сергійович <i.kovalenko@kmzindustries.ua>
2018-09-13 13:13:55 +0300

    Сортировки в узлах

--- kmzfilecabinet/catalog/urls.py
+++ kmzfilecabinet/catalog/urls.py
@@ -7,5 +7,12 @@ app_name = 'catalog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('units', views.units, name='units'),
+    path('units/bynamea', views.unitsnamea, name='units'),
+    path('units/bynamez', views.unitsnamez, name='units'),
+    path('units/bydatenew', views.unitsdatenew, name='units'),
+    path('units/bydateold', views.unitsdateold, name='units'),
+    path('units/byprenamea', views.unitsprenamea, name='units'),
+    path('units/byprenamez', views.unitsprenamez, name='units'),
+
 
 ]
--- kmzfilecabinet/catalog/views.py
+++ kmzfilecabinet/catalog/views.py
@@ -10,12 +10,15 @@ import logging
 logger = logging.getLogger('catalog')
 
 def index(request):
-    logger.info('Index page visited. User: %s' % (request.user))
+    """
+    Главная страница. Выборки: количество узлов, деталей. Лог админки.
+    """
     admin_log = LogEntry.objects.order_by('-action_time')[:25]
     title = "Главная"
     len_unit = Unit.objects.count()
     len_detail = Detail.objects.count()
     # logger.debug(Unit.objects.all())
+    logger.info('"%s" page visited. User: %s' % (title, request.user))
     return TemplateResponse(request, 'index.html', {'output': Unit.objects.order_by('-edit_date'), 
                                                     'title':title, 
                                                     'len_unit':len_unit,
@@ -24,5 +27,62 @@ def index(request):
                                                     })
 
 def units(request):
+    """
+    Главная страница. Выборки: количество узлов, деталей. Лог админки.
+    """
     title = "Узлы"
+    logger.info('"%s" page visited. User: %s' % (title, request.user))
     return TemplateResponse(request, 'units.html', {'output': Unit.objects.order_by('-edit_date'), 'title':title})
+
+def unitsnamea(request):
+    """
+    Узлы. По имени: А-Я
+    """
+    title = "Узлы. По имени: А-Я"
+    logger.info('"%s" page visited. User: %s' % (title, request.user))
+    req = Unit.objects.order_by('name')
+    return TemplateResponse(request, 'unitsnamea.html', {'output': req, 'title':title})
+
+def unitsnamez(request):
+    """
+    Узлы. По имени: Я-А
+    """
+    title = "Узлы. По имени: Я-А"
+    logger.info('"%s" page visited. User: %s' % (title, request.user))
+    req = Unit.objects.order_by('-name')
+    return TemplateResponse(request, 'unitsnamez.html', {'output': req, 'title':title})
+
+def unitsdatenew(request):
+    """
+    Узлы. По дате: сначала новые. Выборки: узлы по дате
+    """
+    title = "Узлы. Сортировка по дате: сначала новые."
+    logger.info('"%s" page visited. User: %s' % (title, request.user))
+    return TemplateResponse(request, 'unitsdatenew.html', {'output': Unit.objects.order_by('edit_date'), 'title':title})
+
+def unitsdateold(request):
+    """
+    Узлы. По дате: сначала старые. Выборки: узлы по дате
+    """
+    title = "Узлы. Сортировка по дате: сначала старые."
+    logger.info('"%s" page visited. User: %s' % (title, request.user))
+    return TemplateResponse(request, 'unitsdateold.html', {'output': Unit.objects.order_by('-edit_date'), 'title':title})
+
+def unitsprenamea(request):
+    """
+    Узлы. По типу: А-Я
+    """
+    title = "Узлы. По типу: А-Я"
+    logger.info('"%s" page visited. User: %s' % (title, request.user))
+    req = Unit.objects.order_by('prename')
+    return TemplateResponse(request, 'unitsprenamea.html', {'output': req, 'title':title})
+
+def unitsprenamez(request):
+    """
+    Узлы. По типу: Я-А
+    """
+    title = "Узлы. По типу: Я-А"
+    logger.info('"%s" page visited. User: %s' % (title, request.user))
+    req = Unit.objects.order_by('-prename')
+    return TemplateResponse(request, 'unitsprenamez.html', {'output': req, 'title':title})
+
